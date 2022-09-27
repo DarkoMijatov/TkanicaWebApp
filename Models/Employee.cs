5af1a70c89118b745fc01d06c3323fcaec51f9c7
DarkoMijatov <darko.mijatov95@gmail.com>
2022-09-28 01:34:31 +0200

    Add Employee and EmployeeMemberGroup models

---
+++ Models/Employee.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using TkanicaWebApp.Interfaces;
+
+namespace TkanicaWebApp.Models
+{
+    public class Employee : ITrackable
+    {
+        public int Id { get; set; }
+        [DataType(DataType.Text)]
+        [Column(TypeName = "varchar(20)")]
+        public string FirstName { get; set; }
+        [DataType(DataType.Text)]
+        [Column(TypeName = "varchar(30)")]
+        public string LastName { get; set; }
+        [DataType(DataType.Text)]
+        [Column(TypeName = "varchar(50)")]
+        public string Title { get; set; }
+        [DataType(DataType.Date)]
+        public DateTime? DateOfBirth { get; set; }
+        [DataType(DataType.Date)]
+        public DateTime StartDate { get; set; }
+        [DataType(DataType.Date)]
+        public DateTime? EndDate { get; set; }
+        public EarningType EarningType { get; set; }
+        public decimal EarningAmount { get; set; }
+        public PayPeriod PayPeriod {get; set; }
+        public decimal? OtherExpenses { get; set; }
+        [DataType(DataType.Text)]
+        [Column(TypeName = "varchar(50)")]
+        public string? OtherExpensesDescription { get; set; }
+        [DataType(DataType.DateTime)]
+        public DateTime CreatedAt { get; set; }
+        [DataType(DataType.DateTime)]
+        public DateTime UpdatedAt { get; set; }
+        public List<EmployeeMemberGroup> EmployeeMemberGroups { get; set; }
+    }
+    public enum EarningType
+    {
+        Fixed = 1,
+        ByRehearsal,
+        ByMember
+    }
+    public enum PayPeriod
+    {
+        Daily = 1,
+        Weekly,
+        Monthly
+    }
+}
